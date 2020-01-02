010fcb0706c760620cdf86c8eb171d861481cf4d
hanjunyu <hanjunyu@kankan.com>
2020-01-02 20:19:00 +0800

    爬楼梯-不相邻数最大值

---
+++ LeetCodeAlg/src/java/com/han/leetcode/dynamicPlan/ClimbStair.java
@@ -0,0 +1,64 @@
+package com.han.leetcode.dynamicPlan;
+
+/**
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ *
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ *
+ * 注意：给定 n 是一个正整数。
+ *
+ * 示例 1：
+ *
+ * 输入： 2
+ * 输出： 2
+ * 解释： 有两种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶
+ * 2.  2 阶
+ * 示例 2：
+ *
+ * 输入： 3
+ * 输出： 3
+ * 解释： 有三种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶 + 1 阶
+ * 2.  1 阶 + 2 阶
+ * 3.  2 阶 + 1 阶
+ *
+ *  * 输入： 4
+ *  * 输出： 3
+ *  * 解释： 有三种方法可以爬到楼顶。
+ *  * 1.  1 阶 + 1 阶 + 1 阶 + 1阶
+ *  * 2.  1 阶 + 2 阶 + 1
+ *  * 3.  2 阶 + 1 阶 + 1
+ *  4. 2 + 2
+ *  5.1+1+2
+ *
+ *  线性规划
+ * 状态转移方程： f(n) = f(n-1) + f(n-2) (f(1) = 1 f(2) =2)
+ */
+public class ClimbStair {
+    public static void main(String[] args) {
+       int n = 10;
+        System.out.println(new ClimbStair().caclulateSteps(n));
+    }
+
+    private  int caclulateStep(int n){
+        if(n == 1)
+            return 1;
+        if(n == 2)
+            return 2;
+        return caclulateStep(n-1) +caclulateStep(n-2);
+    }
+
+    private int  caclulateSteps(int n){
+        int pre = 2;
+        int prepre = 1;
+        int result = 0;
+        for(int i =3;i< n+1;i++){
+            result = pre+prepre;
+            prepre = pre;
+            pre=result;
+        }
+        return result;
+    }
+
+}
